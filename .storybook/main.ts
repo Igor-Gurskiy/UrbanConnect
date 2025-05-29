c2fce23907941271ef3e0cedc0207431d8c324ac
Igor-Gurskiy <iixrug@yandex.ru>
2025-05-29 11:40:51 +0300

    add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+
+  "addons": ['@storybook/addon-links', "@storybook/addon-docs"],
+
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  },
+
+  core: {
+    disableTelemetry: true, // Опционально: отключаем телеметрию
+  }
+};
+export default config;
